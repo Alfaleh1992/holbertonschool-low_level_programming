cd1bd174ddae4c846ed05f2a9d1555cf29ef94ab
Alfaleh1992 <imis.190@gmail.com>
2025-02-19 15:07:17 +0000

    f

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- * _abs - gets the absolute value of an integer
- * @n: the number to check
- * Return: the absolute value
+ * _abs - Computes the absolute value of an integer
+ * @n: The integer to compute the absolute value of
+ *
+ * Return: The absolute value of n
  */
 int _abs(int n)
 {
 	if (n < 0)
-	{
-		if (n == -2147483648)
-			return (2147483647); /* Avoids overflow */
-		return (-n); /* Make it positive */
-	}
-	return (n); /* It's already positive */
+		return (-n);
+	return (n);
 }
